7dfa9441e3c84ddbbad557fc6821d3473693d51b
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-09 20:40:52 +0100

    Update breif

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿/* BREIF:
+ * 
+ * Ask the user for their first name. Continue asking for first names until 
+ * there are no more. Then loop through each name using foreach and tell each 
+ * person hello on the Console.
+ * 
+ */
 
 
 
